b1f27558b0c976ec0970c9147235d907c84d5721
ihayork <90470940+ihayork@users.noreply.github.com>
2022-03-22 13:09:09 -0400

    update

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import random
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    
+    m = open("message.txt","r")
+    messages = m.readlines()
+    numMessage = len(messages)
+    
+    select = random.randint(0, numMessage-1)
+    return render_template("index.html", message=messages[select])
 
     # Load current count
     f = open("count.txt", "r")
@@ -20,6 +28,14 @@ def index():
 
     # Render HTML with count variable
     return render_template("index.html", count=count)
+
+@app.route('/', methods=['POST'])
+def writeMessage():
+    message = request.form["TEXT"]
+    m = open("message.txt","a")
+    m.write("\n" + message)
+    return
+    
 
 if __name__ == "__main__":
     app.run()
