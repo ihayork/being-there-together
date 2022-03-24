5f62b7966b89b9b07ba3980a26d5f1b2ecfb738d
ihayork <90470940+ihayork@users.noreply.github.com>
2022-03-24 16:33:18 -0400

    yes

--- app.py
+++ app.py
@@ -3,39 +3,30 @@ import random
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
+    
+    if(request.method == 'POST'):
+        message = request.form['TEXT']
+        m = open("message.txt","a")
+        m.write("\n" + message)
+        m.close()
     
     m = open("message.txt","r")
     messages = m.readlines()
     numMessage = len(messages)
+    m.close()
     
     select = random.randint(0, numMessage-1)
-    return render_template("index.html", message=messages[select])
-
-    # Load current count
+    
     f = open("count.txt", "r")
-    count = int(f.read())
-    f.close()
-
-    # Increment the count
-    count += 1
-
-    # Overwrite the count
+    count = int(f.read()) + 1
     f = open("count.txt", "w")
     f.write(str(count))
     f.close()
 
     # Render HTML with count variable
-    return render_template("index.html", count=count)
-
-@app.route('/', methods=['POST'])
-def writeMessage():
-    message = request.form["TEXT"]
-    m = open("message.txt","a")
-    m.write("\n" + message)
-    return
-    
+    return render_template("index.html", message=messages[select], count=count) 
 
 if __name__ == "__main__":
     app.run()
